28f0fb881a804918a6260c24094d90d95a87e88a
Kaushik Prakash <k2.prakash@gmail.com>
2016-09-13 18:03:59 -0600

    Checking in the files memory.c and memory.h. Need to unit test with the (length <=0 ) conditions commented. Had unit tested with (length<=0) condition uncommented and the code works great.

--- include/memory.h
+++ include/memory.h
@@ -1,6 +1,10 @@
 #ifndef _MEMORY_H
 #define _MEMORY_H
 
+static const uint8_t SUCCESS = 0;
+static const int8_t NULL_PTR_ERROR = -1;
+static const int8_t INVAL_LEN_ERROR = -2;
+static const int8_t ADDR_OVERLAP_ERROR = -3;
 /*
 This fucntion will move byte(s) of data starting from the source address location to the desitination address location.
 */
@@ -17,4 +21,6 @@ This fucntion will takes a pointer to a memory location and a length in bytes an
 */
 
 int8_t my_reverse(uint8_t * src, uint32_t length);
+
+uint32_t srclen(uint8_t *src);
 #endif
