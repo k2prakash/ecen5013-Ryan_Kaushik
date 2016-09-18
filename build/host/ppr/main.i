69d478f749212ce9e1cca97cb1e3765189e1af09
jet <ryan.haun@colorado.edu>
2016-09-18 04:28:22 -0600

    Removing build files

---
+++ build/host/ppr/main.i
@@ -0,0 +1,21 @@
+# 1 "src/main.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 1 "<command-line>" 2
+# 1 "src/main.c"
+# 1 "src/../include/project_1.h" 1
+
+
+
+
+
+
+void project_1_report();
+# 2 "src/main.c" 2
+
+int main()
+{
+    project_1_report();
+    return 0;
+}
--- sources.mk
+++ sources.mk
@@ -1,14 +1,22 @@
 #VARIABLE DEFINITIONS
 
 #source and includes
-VPATH := src include
-#build outputs
-BLDDIR := build
+VPATH := src include sdir odir idir
+cdir := src
+bdir := build/$(platform)/bin
+odir := build/$(platform)/obj
+sdir := build/$(platform)/asm
+idir := build/$(platform)/ppr
 #lists of includes, source and object files
 inc := project_1.h memory.h data.h
 src := main.c $(inc:.h=.c)
+cfiles := $(addprefix $(idir)/,$(src))
 obj := $(src:.c=.o)
+ofiles := $(addprefix $(idir)/,$(obj))
 asm := $(src:.c=.S)
+sfiles := $(addprefix $(idir)/,$(asm))
 ppr := $(src:.c=.i)
+ifiles := $(addprefix $(idir)/,$(ppr))
+
 #the  final output file name
-OUTPUT := project
+output := project
